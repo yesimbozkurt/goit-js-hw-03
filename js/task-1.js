f5424df27719d4275fbc04ea627af06cdbe4c609
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-01-14 22:25:10 +0300

    word puzzle

--- js/task-1.js
+++ js/task-1.js
@@ -13,3 +13,15 @@ console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 300
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
 console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+
+function slugify (title) {
+  return title.toLowerCase().split(" ").join("-");
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
--- js/task-3.js
+++ js/task-3.js
@@ -14,3 +14,22 @@ console.log(checkForSpam("Amazing SalE, only tonight!")); // true
 console.log(checkForSpam("Trust me, this is not a spam message")); // true
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+function filterArray(numbers, value) {
+  const newArray = [];
+
+  for (const number of numbers) {
+    if (number > value) {
+      newArray.push(number);
+    }
+  }
+
+  return newArray;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
