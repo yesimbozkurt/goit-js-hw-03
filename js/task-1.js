48b806e06a313b0d294ea7f500f2ed93117c84b8
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-01-19 22:37:35 +0300

    düzeltme

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 function slugify(title) {
   let slug = title.toLowerCase()
   slug = slug.split(" ")
-  slug = slug.join("\t") 
+  slug = slug.join("-") 
   return slug;
 }
 
@@ -9,5 +9,3 @@ console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-
